491274e0091236ecdecf1119fae34df93d5ebd27
Ariel Saldana <ariel@unreal.io>
2023-05-13 21:21:56 -0400

    add navigation component : navigation-bar.module.scss / navigation-bar.tsx

---
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -0,0 +1,22 @@
+import styles from "./navigation-bar.module.scss"
+
+export default function NavigationBar() {
+    return (
+        <div className={styles.navigationBar}>
+            <div className={styles.navigationBarContents}>
+                <div className={styles.logo}>
+                    <a>
+                        <img src="/ariel-logo.svg" alt="Ariel Logo"/>
+                    </a>
+
+                </div>
+                <nav>
+                    <ol>
+                        <li><a href="/blogs">Blogs</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    )
+}
