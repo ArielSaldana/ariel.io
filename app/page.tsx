7f58e8a572ecfb18fd6068cbf675ca4870a2f107
Ariel Saldana <ariel@unreal.io>
2024-06-24 22:23:30 -0400

    Initial Commit ✨

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,28 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
+import Image from "next/image";
+import logo from "../public/assets/svg/light-fg.svg";
 
 export default function Page() {
   return (
     <section>
+      <div>
+        <Image
+          src={logo}
+          alt="Ariel Saldana"
+          width={200}
+          height={200}
+          className="rounded-full"
+        />
+      </div>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Ariel Saldana
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a full-stack software engineer based out of NYC, bringing over a decade of experience from traditional finance to the forefront of AI and Web3 technologies. My extensive background empowers my approach to innovative software solutions, seamlessly integrating complex systems from conventional financial frameworks into the cutting-edge spaces of AI and blockchain.`}
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
