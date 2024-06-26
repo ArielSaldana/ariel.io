3b7dbb3189f5539ee3dea1ea0efee35a50ed2aa0
Ariel Saldana <ariel@unreal.io>
2024-06-26 03:47:13 -0400

    Fix casing which breaks build in some instances

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import WhiteLogo from "@/app/assets/logo-white.svg";
-import styles from "./Logo.module.css";
+import styles from "./logo.module.css";
 
 export default function Logo() {
   const logoStyle = styles.logo + " max-w-36 mb-6";
