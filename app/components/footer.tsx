4ad365167e414af98cc32467508c31cfc9ff2c0e
Ariel Saldana <ariel@unreal.io>
2024-06-26 13:26:14 -0400

    Adding proper social media links to the footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,33 @@
+import XLogo from "@/public/assets/svg/twitterx.svg";
+import InstagramLogo from "@/public/assets/svg/instagram.svg";
+import GithubLogo from "@/public/assets/svg/github.svg";
+
+const footerLinks = [
+  {
+    title: "rss",
+    link: "/rss",
+  },
+  {
+    title: "This site's source code",
+    link: "https://github.com/arielsaldana/ariel.io",
+  },
+];
+
+const socialMediaLinks = [
+  {
+    icon: XLogo,
+    link: "https://x.com/arielsaldana_",
+  },
+  {
+    icon: GithubLogo,
+    link: "https://github.com/arielsaldana",
+  },
+  {
+    icon: InstagramLogo,
+    link: "https://instagram.com/arielsaldana_",
+  },
+];
+
 function ArrowIcon() {
   return (
     <svg
@@ -18,30 +48,37 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/arielsaldana"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
-          </a>
-        </li>
-      </ul>
+      <div className="flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 justify-between">
+        <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+          {footerLinks.map((footerLink) => (
+            <li key={footerLink.link}>
+              <a
+                className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+                rel="noopener noreferrer"
+                target="_blank"
+                href={footerLink.link}
+              >
+                <ArrowIcon />
+                <p className="ml-2 h-7">{footerLink.title}</p>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+          {socialMediaLinks.map((socialMediaLink) => (
+            <li key={socialMediaLink.link} className="h-5 w-5">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="h-5 w-5 dark:fill-white"
+                href={socialMediaLink.link}
+              >
+                <socialMediaLink.icon />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
       </p>
