961615d5e84cb18cae72ee3d529e1247540dd64d
Ariel Saldana <ariel@unreal.io>
2024-06-27 04:33:22 -0400

    Update intro to sound less pretentious and highlight interests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <Logo />
       </div>
       <p className="mb-4">
-        {`Hi! I'm Ariel, a full-stack Software Engineer and Computer Scientist based in NYC. With over a decade of experience in FinTech, I'm passionate about leveraging technology to create a positive impact. Currently, I'm diving into the exciting worlds of Decentralized Finance (DeFi) and Artificial Intelligence (AI), exploring innovative ways to make the world a better place. Join me on this journey as I blend my expertise in traditional finance with cutting-edge tech to build a smarter, more decentralized future.`}
+        {`Hi! I'm Ariel, a full-stack Software Engineer and Computer Scientist based in NYC. With over a decade of experience in FinTech, I'm passionate about leveraging technology to create a positive impact. Currently, I'm diving into the exciting worlds of Decentralized Finance (DeFi) and Artificial Intelligence (AI), exploring innovative ways to make the world a better place.`}
       </p>
       <div className="my-8">
         <BlogPosts />
