92c52194ea5a1beffa04bb792eba42a159d84972
Ariel Saldana <ariel@unreal.io>
2024-06-27 04:11:16 -0400

    Add 'active' class to nav link. Add orange accent to title links in mdx

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const navItems = {
   "/": {
@@ -10,6 +13,7 @@ const navItems = {
 };
 
 export function Navbar() {
+  const pathName = usePathname();
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
@@ -19,11 +23,12 @@ export function Navbar() {
         >
           <div className="flex flex-row space-x-0 group">
             {Object.entries(navItems).map(([path, { name }]) => {
+              const isActive = pathName === path;
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-oj dark:hover:text-oj group-hover:text-neutral-200 dark:group-hover:text-neutral-800 flex align-middle relative py-2 px-3 hover:text-oj font-medium"
+                  className={`transition-all hover:text-oj dark:hover:text-oj group-hover:text-neutral-200 dark:group-hover:text-neutral-700 flex align-middle relative py-2 px-3 hover:text-oj font-medium ${isActive ? "underline" : ""}`}
                 >
                   {name}
                 </Link>
